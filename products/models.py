053372b614004978b44c383887d8f7dbd2f4aebf
Eric Silva <ericsilvap13@gmail.com>
2025-12-19 10:10:27 -0300

    refactor: Product fields now are decimal; remove status field

--- products/models.py
+++ products/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=65)
-    price = models.IntegerField()
-    quantity = models.IntegerField()
+    price = models.DecimalField(decimal_places=2)
+    quantity = models.FloatField()
     quantity_unit = models.CharField(max_length=65)
-    status = models.CharField(max_length=65)
     cover = models.ImageField(
         upload_to="products/covers/%Y/%m/%d/", blank=True, default=""
     )
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
