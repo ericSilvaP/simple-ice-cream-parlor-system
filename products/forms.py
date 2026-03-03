d42e63ee850f1e9040e1437326306dd1052ee3ed
Eric Silva <ericsilvap13@gmail.com>
2026-03-02 22:49:48 -0300

    feat: simple user form

---
+++ products/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email", "username", "password"]
+
+    password_repeat = forms.CharField(label="Confirmar Senha")
