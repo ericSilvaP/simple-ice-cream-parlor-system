0b917f993f010c9ae1c6d5ee180ba6d613ca4ed6
Eric Silva <ericsilvap13@gmail.com>
2026-03-03 21:22:07 -0300

    feat: user form base

--- products/forms.py
+++ products/forms.py
@@ -8,3 +8,17 @@ class RegisterUserForm(forms.ModelForm):
         fields = ["email", "username", "password"]
 
     password_repeat = forms.CharField(label="Confirmar Senha")
+
+    email = forms.EmailField(label="Email")
+    username = forms.CharField(label="Nome")
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get("password")
+        password_repeat = cleaned_data.get("password_repeat")
+
+        if password != password_repeat:
+            raise forms.ValidationError("Senhas devem ser iguais")
+
+        return cleaned_data
